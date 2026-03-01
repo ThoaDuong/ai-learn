a511cb37d6850518947cd6c2b8523c2b4f130d3a
Thoa Duong <kimthoa2598@gmail.com>
2026-03-01 14:10:51 +0700

    fix extension logo

--- extension/content.js
+++ extension/content.js
@@ -21,13 +21,7 @@
 
         const icon = document.createElement("div");
         icon.id = "tlearn-ext-icon";
-        icon.innerHTML = `
-            <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#6366f1" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20"/>
-                <path d="M8 7h6"/>
-                <path d="M8 11h8"/>
-            </svg>
-        `;
+        icon.innerHTML = `<img src="${chrome.runtime.getURL("icons/icon48.png")}" width="22" height="22" style="border-radius: 4px;" alt="TLearn">`;
         icon.title = "Translate with TLearn";
         document.documentElement.appendChild(icon);
         floatingIcon = icon;
